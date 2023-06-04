6b341c29cf0c90e5dd877a342b1a321f21f2c56c
Alexander James Cole <aj@co-le.com>
2023-06-04 21:30:59 +0200

    feat: recreate strapi project to handle mysql

--- strapi/src/api/home-hero-container/controllers/home-hero-container.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * home-hero-container controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::home-hero-container.home-hero-container');
